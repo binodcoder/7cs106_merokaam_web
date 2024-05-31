f0a7cf56a75a557caba84d9ba839b678aded7a47
binod <binodcoder@gmail.com>
2024-05-31 14:34:45 +0100

    project initialized

---
+++ src/main/java/com/binodcoder/merokaam/MerokaamApplication.java
@@ -0,0 +1,13 @@
+package com.binodcoder.merokaam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerokaamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerokaamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binodcoder/merokaam/MerokaamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binodcoder.merokaam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerokaamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
