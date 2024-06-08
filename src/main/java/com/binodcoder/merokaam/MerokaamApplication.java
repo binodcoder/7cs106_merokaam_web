1311942421d8e58e310be2ff213a9307acefa0b3
binod <binodcoder@gmail.com>
2024-06-08 16:04:36 +0100

    support for spring security added

--- src/main/java/com/binodcoder/merokaam/MerokaamApplication.java
+++ src/main/java/com/binodcoder/merokaam/MerokaamApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.binodcoder.merokaam;
+package com.binodcoder.merokaam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/binodcoder/merokaam/rest/FunRestController.java
+++ src/main/java/com/binodcoder/merokaam/rest/FunRestController.java
@@ -1,4 +1,4 @@
-package main.java.com.binodcoder.merokaam.rest;
+package com.binodcoder.merokaam.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
