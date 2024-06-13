370c42a7e31c32b4dc88fae844adb034967bd7e8
binod <binodcoder@gmail.com>
2024-06-13 15:24:50 +0100

    global exception handling

--- src/main/java/com/binodcoder/merokaam/rest/TeacherRestController.java
+++ src/main/java/com/binodcoder/merokaam/rest/TeacherRestController.java
@@ -39,27 +39,4 @@ public class TeacherRestController {
         return theTeachers.get(teacherId);
     }
 
-    //Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<TeacherErrorResponse> handleException(TeacherNotFoundException exc) {
-        //create a TeacherErrorResponse
-        TeacherErrorResponse error = new TeacherErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    //add another exception handler .. to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<TeacherErrorResponse> handleException(Exception exc){
-        //create a TeacherErrorResponse
-        TeacherErrorResponse error = new TeacherErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
