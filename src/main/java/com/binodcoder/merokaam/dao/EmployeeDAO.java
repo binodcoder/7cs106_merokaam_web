5bf4eb6d8a1bb90cf3d5362d063de6ba8f69aaff
binod <binodcoder@gmail.com>
2024-06-14 09:03:58 +0100

    employee DAO added

---
+++ src/main/java/com/binodcoder/merokaam/dao/EmployeeDAO.java
@@ -0,0 +1,14 @@
+package com.binodcoder.merokaam.dao;
+
+
+import com.binodcoder.merokaam.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> findAll();
+    Employee findById(int theId);
+    Employee save(Employee theEmployee);
+    void deleteById(int theId);
+}
+
