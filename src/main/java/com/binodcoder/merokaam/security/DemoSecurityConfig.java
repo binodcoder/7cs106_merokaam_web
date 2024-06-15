bfbd6cf6f2273df40c9c3fece11d64fb7dd02095
binod <binodcoder@gmail.com>
2024-06-15 21:17:02 +0100

    REST API security -custom tables

--- src/main/java/com/binodcoder/merokaam/security/DemoSecurityConfig.java
+++ src/main/java/com/binodcoder/merokaam/security/DemoSecurityConfig.java
@@ -18,7 +18,12 @@ public class DemoSecurityConfig {
     // add support for JDBC .. no more hardcoded users :-)
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+        //define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+        return jdbcUserDetailsManager;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
