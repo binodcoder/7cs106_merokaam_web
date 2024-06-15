33d3df03bc4c7604d814c538bd660462f5f8877b
binod <binodcoder@gmail.com>
2024-06-15 16:29:49 +0100

    REST security-basic configuration

---
+++ src/main/java/com/binodcoder/merokaam/security/DemoSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.binodcoder.merokaam.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john= User.builder()
+                .username("john")
+                .password("{noop}john123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary= User.builder()
+                .username("mary")
+                .password("{noop}mary123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan= User.builder()
+                .username("susan")
+                .password("{noop}susan123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+}
