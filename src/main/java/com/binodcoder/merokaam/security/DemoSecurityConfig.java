354bc810f44af5d57c48790949c706000c383b43
binod <binodcoder@gmail.com>
2024-06-15 18:38:38 +0100

    JDBC authentication

--- src/main/java/com/binodcoder/merokaam/security/DemoSecurityConfig.java
+++ src/main/java/com/binodcoder/merokaam/security/DemoSecurityConfig.java
@@ -7,33 +7,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+    // add support for JDBC .. no more hardcoded users :-)
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john= User.builder()
-                .username("john")
-                .password("{noop}john123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary= User.builder()
-                .username("mary")
-                .password("{noop}mary123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan= User.builder()
-                .username("susan")
-                .password("{noop}susan123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configurer->
