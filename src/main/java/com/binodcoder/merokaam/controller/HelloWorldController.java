f98581d99a2f1e6e70815c39fb72b55f316c1e64
binod <binodcoder@gmail.com>
2024-06-16 11:13:56 +0100

    adding data to spring MVC model

--- src/main/java/com/binodcoder/merokaam/controller/HelloWorldController.java
+++ src/main/java/com/binodcoder/merokaam/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.binodcoder.merokaam.controller;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +19,25 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm(){
         return "helloworld";
+    }
+
+    //need a controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+        //read the request parameter from the HTML form
+        String theName=request.getParameter("studentName");
+
+        //convert the data to all caps
+        theName=theName.toUpperCase();
+
+        //create the message
+        String result="Yo! "+theName;
+
+        //add message to the Model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+
     }
 }
