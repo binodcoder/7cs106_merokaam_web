13664cd4a11e95cbce95a616e823244700d30433
binod <binodcoder@gmail.com>
2024-06-16 11:21:15 +0100

    spring MVC binding request params

--- src/main/java/com/binodcoder/merokaam/controller/HelloWorldController.java
+++ src/main/java/com/binodcoder/merokaam/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -39,5 +40,23 @@ public class HelloWorldController {
 
         return "helloworld";
 
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String makeUpperCase(
+            @RequestParam("studentName") String theName,
+            Model model
+    ){
+        // now we can use the variable theName
+        //convert the data to all caps
+        theName=theName.toUpperCase();
+
+        //create the message
+        String result="Yo! "+theName;
+
+        //add message to the Model
+        model.addAttribute("message", result);
+
+        return "helloworld";
     }
 }
