0b74b54c1f063b7a48e0549fb9d4b0614ad3a7ab
binod <binodcoder@gmail.com>
2024-07-05 17:11:40 +0100

    setup fresh project

---
+++ src/main/java/com/binodcoder/merokaam/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.binodcoder.merokaam.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
--- src/main/java/com/binodcoder/merokaam/controller/TestController.java
+++ src/main/java/com/binodcoder/merokaam/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String showHome(){
         return "home";
     }
