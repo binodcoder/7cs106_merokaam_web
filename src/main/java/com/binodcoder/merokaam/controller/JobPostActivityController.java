a51e1e66b9b6efdcae3cb2181164c9f80732196b
Binod Bhandari <binodcoder@gmail.com>
2024-07-08 22:47:52 +0100

    edit a job

--- src/main/java/com/binodcoder/merokaam/controller/JobPostActivityController.java
+++ src/main/java/com/binodcoder/merokaam/controller/JobPostActivityController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
@@ -27,7 +28,7 @@ public class JobPostActivityController {
         this.jobPostActivityService = jobPostActivityService;
     }
 
-    @GetMapping("/dashboard/")
+    @GetMapping("/dashboard")
     public String searchJobs(Model model) {
         Object currentUserProfile = usersService.getCurrentUserProfile();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -59,6 +60,14 @@ public class JobPostActivityController {
         jobPostActivity.setPostedDate(new Date());
         model.addAttribute("jobPostActivity", jobPostActivity);
         //  jobPostActivityService.addNew(jobPostActivity);
-        return "redirect:/dashboard";
+        return "redirect:/dashboard/";
+    }
+
+    @PostMapping("dashboard/edit/{id}")
+    public String editJob(@PathVariable("id") int id, Model model) {
+        JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "add-jobs";
     }
 }
