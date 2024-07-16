e1c8f9dcaaf35df81a227ff38b6dd02f4dd0b112
Binod Bhandari <binodcoder@gmail.com>
2024-07-16 18:35:58 +0100

    added some code on controller for searching and applying for job

--- src/main/java/com/binodcoder/merokaam/service/RecruiterProfileService.java
+++ src/main/java/com/binodcoder/merokaam/service/RecruiterProfileService.java
@@ -1,6 +1,12 @@
 package com.binodcoder.merokaam.service;
 import com.binodcoder.merokaam.entity.RecruiterProfile;
+import com.binodcoder.merokaam.entity.Users;
 import com.binodcoder.merokaam.repository.RecruiterProfileRepository;
+import com.binodcoder.merokaam.repository.UsersRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,9 +15,11 @@ import java.util.Optional;
 public class RecruiterProfileService {
 
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final UsersRepository usersRepository;
 
-    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository, UsersRepository usersRepository) {
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Optional<RecruiterProfile> getOne(Integer id){
@@ -20,5 +28,15 @@ public class RecruiterProfileService {
 
     public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
         return recruiterProfileRepository.save(recruiterProfile);
+    }
+
+    public RecruiterProfile getCurrentRecruiterProfile() {
+      Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+      if(!(authentication instanceof AnonymousAuthenticationToken)){
+          String currentUsername=authentication.getName();
+        Users users= usersRepository.findByEmail(currentUsername).orElseThrow(()-> new UsernameNotFoundException("User not "+ "found"));
+       Optional<RecruiterProfile> recruiterProfile=   getOne(users.getUserId());
+       return  recruiterProfile.orElseThrow(null);
+      }else return null;
     }
 }
